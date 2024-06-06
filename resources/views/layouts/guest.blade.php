41ee2bfad620ca49a3cb0eb2f2967daa7a71d8be
Kheven Bitoon <kbitoon@gmail.com>
2024-06-07 01:13:19 +0800

    change logo to text

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,11 +16,16 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/" wire:navigate>
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
+{{--            <div>--}}
+{{--                <a href="/" wire:navigate>--}}
+{{--                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />--}}
+{{--                </a>--}}
+                <div class="relative inline-flex items-center justify-center w-40 h-40 overflow-hidden bg-red-100 rounded-full dark:bg-red-600">
+                    <a href="/" wire:navigate>
+                        <span class="text-6xl font-medium text-gray-600 dark:text-gray-300">BIS</span>
+                    </a>
+                </div>
+{{--            </div>--}}
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
