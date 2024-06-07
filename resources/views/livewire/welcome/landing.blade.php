34fe00111a78d141c41161b394f617f30e0ee5b8
winstonpepito <winston.pepito@gmail.com>
2024-06-07 09:38:49 +0800

    Fixed category icon error

--- resources/views/livewire/welcome/landing.blade.php
+++ resources/views/livewire/welcome/landing.blade.php
@@ -10,7 +10,9 @@
                     <div class="relative flex items-center gap-6 lg:items-end">
                         <div id="docs-card-content" class="flex items-start gap-6 lg:flex-col">
                             <div class="flex size-12 shrink-0 items-center justify-center rounded-full bg-[#FF2D20]/10 sm:size-16">
+                            @if(isset($pinned_announcement->category->icon))
                                 {!! $pinned_announcement->category->icon !!}
+                            @endif
                             </div>
 
                             <div class="pt-3 sm:pt-5 lg:pt-0">
@@ -52,7 +54,9 @@
                     class="flex items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
                 >
                     <div class="flex size-12 shrink-0 items-center justify-center rounded-full bg-[#FF2D20]/10 sm:size-16">
+                    @if(isset($pinned_announcement->category->icon))
                         {!! $pinned_announcement->category->icon !!}
+                    @endif
                     </div>
 
                     <div class="pt-3 sm:pt-5">
