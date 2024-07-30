02d2bea01246fdb2934a96e8cc1cda54a42de40f
unknown <graceann151@gmail.com>
2024-07-30 13:52:20 -0400

    fixed minor bug

--- resources/views/livewire/forms/vehicle-schedule-form.blade.php
+++ resources/views/livewire/forms/vehicle-schedule-form.blade.php
@@ -20,7 +20,7 @@
             
             <div class="mt-4">
                 <x-input-label for="vehicle_id" :value="__('Vehicle')" />
-                <select wire:model.live.debounce.500ms="form.vehicle_id" id="vehicle_id" class="mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
+                <select wire:model.live.debounce.250ms="form.vehicle_id" id="vehicle_id" class="mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
                     <option selected>Please select a vehicle</option>
                     @forelse($vehicles as $vehicle)
                         <option value="{{ $vehicle->id }}">{{ $vehicle->name }}</option>
@@ -33,7 +33,7 @@
 
             <div class="mt-4">
                 <x-input-label for="driver_id" :value="__('Driver')" />
-                <select wire:model.live.debounce.500ms="form.driver_id" id="driver_id" class="mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
+                <select wire:model.live.debounce.250ms="form.driver_id" id="driver_id" class="mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
                     <option selected>Please select a driver</option>
                     @forelse($drivers as $driver)
                         <option value="{{ $driver->id }}">{{ $driver->name }}</option>
