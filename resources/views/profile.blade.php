063c6dd31f37f52c267cb69ac737d73ff27e7fae
unknown <graceann151@gmail.com>
2024-09-20 13:27:29 -0400

    minor changes

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -13,11 +13,7 @@
                 </div>
             </div>
 
-            <!-- <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    <livewire:profile.update-personal-information />
-                </div>
-            </div> -->
+
 
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
