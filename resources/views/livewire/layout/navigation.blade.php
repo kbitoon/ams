8583a74a94d436a758688ff4b706ef92243ee73e
unknown <graceann151@gmail.com>
2024-09-30 14:20:18 -0400

    fixed nav bug

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -26,7 +26,7 @@ new class extends Component
 {{--                    <a href="{{ route('dashboard') }}" wire:navigate>--}}
 {{--                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />--}}
 {{--                    </a>--}}
-                    <div class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-red-100 rounded-full dark:bg-red-600">
+                    <div class="inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-red-100 rounded-full dark:bg-red-600">
                         <a href="/">
                         {{--                            <span class="font-medium text-gray-600 dark:text-gray-300">BIS</span> --}}
                         <img src="/storage/public/logo.png" />
@@ -129,7 +129,7 @@ new class extends Component
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <img class="w-10 h-9 rounded-full" src="{{ auth()->user()->photoUrl() }}">
-                <x-dropdown align="right" width="48" class="relative z-20">
+                <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
                             <div x-data="{{ json_encode(['name' => auth()->user()->name]) }}" x-text="name" x-on:profile-updated.window="name = $event.detail.name"></div>
