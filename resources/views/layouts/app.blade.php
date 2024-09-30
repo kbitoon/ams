638bfd22debd25b600469beefae3bd61c2caecb8
unknown <graceann151@gmail.com>
2024-09-30 16:34:28 -0400

    minor changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,6 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
-        <meta http-equiv="Pragma" content="no-cache" />
-        <meta http-equiv="Expires" content="0" />
 
         <title>{{ config('app.name', 'Bacayan Information System') }}</title>
 
