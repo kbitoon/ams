fa908fe6684d2e191d3ee1d02f18455eadf42c03
unknown <graceann151@gmail.com>
2024-10-05 15:55:28 -0400

    changed the track button color

--- resources/views/track-complaint.blade.php
+++ resources/views/track-complaint.blade.php
@@ -31,7 +31,7 @@
                                 </div>
 
                                 <div>
-                                    <button type="submit" class="inline-flex items-center justify-center px-4 py-2 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+                                    <button type="submit" class="inline-flex items-center justify-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
                                         {{ __('Track') }}
                                     </button>
                                 </div>
