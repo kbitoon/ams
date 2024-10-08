391cc195a8cef64e6980f181297c02fe699ee271
unknown <graceann151@gmail.com>
2024-10-08 05:26:34 -0300

    minor changes

--- resources/views/livewire/complaint/view.blade.php
+++ resources/views/livewire/complaint/view.blade.php
@@ -5,7 +5,13 @@
     <div class="pt-3 sm:pt-5">
         <h3 class="text-2xl font-bold text-gray-900 dark:text-gray-100">{!! $complaint->title !!}</h3>
         <div class="mt-4 text-sm/relaxed text-gray-800 dark:text-gray-300 space-y-2">
-            <p><span class="font-semibold">{!! $complaint->name !!} | {!! $complaint->contact_number !!}</span><br><span class ="block text-gray-500 dark:text-gray-400" style="font-size: 0.65rem;">{{ $complaint->created_at->format('F j, Y') }}</span></p>
+        <p>
+            <span class="font-semibold">
+                {!! $complaint->name !!}
+                @if ($complaint->contact_number)
+                | {!! $complaint->contact_number !!}
+                @endif
+                </span>
             <p>{!! $complaint->content !!}</p>
             @if ($complaint->status === 'Done')
                 <p><span class="font-semibold">Approved By: </span> {!! $complaint->user->name !!}</p>
@@ -14,15 +20,15 @@
         <!-- Attachments Section -->
         <div class="mt-4 text-sm/relaxed text-gray-800 dark:text-gray-300 space-y-2">
             <h3 class="font-semibold">Attachments</h3>
-            <div class="space-y-2">
+            <ul class="list-disc list-inside space-y-2">
                 @foreach($complaint->assets as $attachment)
-                    <div>
+                    <li>
                         <a href="{{ Storage::url($attachment->path) }}" target="_blank" class="text-blue-500 hover:underline">
                             {{ basename($attachment->path) }}
                         </a>
-                    </div>
+                    </li>
                 @endforeach
-            </div>
+            </ul>
         </div>
         
         <div class="mt-4 text-sm/relaxed text-gray-800 dark:text-gray-300 space-y-2">
