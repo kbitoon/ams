1ed0596ee7bfffcd7d28bf6ff70b95e03fee365a
unknown <graceann151@gmail.com>
2024-12-02 15:01:18 +0800

    fixed date time format

--- resources/views/livewire/vehicle/calendar.blade.php
+++ resources/views/livewire/vehicle/calendar.blade.php
@@ -14,12 +14,30 @@
             eventLimit: 5,
 
             eventClick: function(event) {
+                var startDate = new Date(event.start);
+                var endDate = event.end ? new Date(event.end) : null;
+
+                // Function to format the time as 12-hour AM/PM in UTC
+                function formatUTCDate(date) {
+                    var hours = date.getUTCHours();
+                    var minutes = date.getUTCMinutes();
+                    var ampm = hours >= 12 ? 'PM' : 'AM';
+                    hours = hours % 12;
+                    hours = hours ? hours : 12; // The hour '0' should be '12'
+                    minutes = minutes < 10 ? '0' + minutes : minutes;
+                    return hours + ':' + minutes + ' ' + ampm;
+                }
+
+                // Format start and end dates in UTC without timezone conversion
+                var formattedStart = (startDate.getUTCMonth() + 1) + '/' + startDate.getUTCDate() + '/' + startDate.getUTCFullYear() + ' ' + formatUTCDate(startDate);
+                var formattedEnd = endDate ? (endDate.getUTCMonth() + 1) + '/' + endDate.getUTCDate() + '/' + endDate.getUTCFullYear() + ' ' + formatUTCDate(endDate) : 'N/A';
+
                 var details = `
                     Destination: ${event.title || 'N/A'}\n
-                    Vehicle: ${event.vehicle|| 'N/A'}\n
-                    Driver: ${event.driver|| 'N/A'}\n
-                    Schedule Start: ${event.formatted_start || event.start}\n
-                    Schedule End: ${event.formatted_end || event.end || 'N/A'}\n
+                    Vehicle: ${event.vehicle || 'N/A'}\n
+                    Driver: ${event.driver || 'N/A'}\n
+                    Schedule Start: ${formattedStart}\n
+                    Schedule End: ${formattedEnd}\n
                 `;
                 alert(details);
             },
