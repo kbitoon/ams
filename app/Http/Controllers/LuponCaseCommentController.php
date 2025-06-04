e39096c7bda9e1017684ee93b17ff54fa54537be
Grace An Velayo <graceann151@gmail.com>
2025-06-04 14:05:58 +0800

    minor fix

--- app/Http/Controllers/LuponCaseCommentController.php
+++ app/Http/Controllers/LuponCaseCommentController.php
@@ -46,19 +46,17 @@ class LuponCaseCommentController extends Controller
         $summonedDate = $request->query('summoned_date');
         $dateIssued = $request->query('date_issued');
 
-        $luponSummon = LuponSummonTracking::with('luponCase.luponCaseComplainants', 'luponCase.luponCaseRespondents')
-            ->where('lupon_case_id', $luponCaseId)
+        // Always get the LuponCase
+        $luponCase = LuponCase::with(['luponCaseComplainants', 'luponCaseRespondents'])->findOrFail($luponCaseId);
+
+        // Get the latest summon if it exists, but it's optional
+        $luponSummon = LuponSummonTracking::where('lupon_case_id', $luponCaseId)
             ->latest()
             ->first();
 
-        if (!$luponSummon) {
-            return response("No summon record found for Lupon Case ID $luponCaseId", 404);
-        }
-
-        $luponCase = $luponSummon->luponCase;
         $pdfContent = \App\Models\PdfContent::latest()->first();
 
-        // Pass the dates to the view
+        // Pass the dates and both models to the view
         $pdf = Pdf::loadView('pdf.summon', compact('luponSummon', 'luponCase', 'pdfContent', 'summonedDate', 'dateIssued'));
 
         return $pdf->download("lupon_summon_{$luponCase->case_no}.pdf");
