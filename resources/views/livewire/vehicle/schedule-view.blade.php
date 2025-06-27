0b7c86d5827b0cc508e3302b70c85c616b811197
Grace An Velayo <graceann151@gmail.com>
2025-06-27 11:44:28 +0800

    minor changes

--- resources/views/livewire/vehicle/schedule-view.blade.php
+++ resources/views/livewire/vehicle/schedule-view.blade.php
@@ -1,16 +1,45 @@
-<div class="bg-white dark:bg-gray-800 shadow-md rounded-lg p-6 h-auto">
-    <button class="absolute top-2 right-2 text-gray-600 hover:text-gray-800 focus:outline-none text-3xl" wire:click="closeModal">
+<div class="relative bg-white dark:bg-gray-800 shadow-md rounded-lg p-6">
+    <button
+        class="absolute top-2 right-2 text-gray-600 hover:text-gray-800 dark:hover:text-gray-200 focus:outline-none text-2xl"
+        wire:click="closeModal"
+        aria-label="Close">
         &times;
     </button>
+
     <div class="pt-3 sm:pt-5">
-        <h3 class="text-2xl font-bold text-gray-900 dark:text-gray-100">Details</h3>
-        
-        <table class="mt-4 text-sm text-gray-800 dark:text-gray-300">
+        <h3 class="text-2xl font-bold text-gray-900 dark:text-gray-100 mb-4">
+            {{ $vehicleSchedule->destination }}
+        </h3>
+
+        <table class="text-sm text-left text-gray-800 dark:text-gray-300">
             <tbody>
                 <tr>
-                    <td class="py-2">{!! $vehicleSchedule->details !!}</td>
+                    <th class="py-1 pr-2 font-medium whitespace-nowrap">Name:</th>
+                    <td class="py-1">testing</td>
+                </tr>
+                <tr>
+                    <th class="py-1 pr-2 font-medium whitespace-nowrap">Vehicle:</th>
+                    <td class="py-1">testing</td>
+                </tr>
+                <tr>
+                    <th class="py-1 pr-2 font-medium whitespace-nowrap">Driver:</th>
+                    <td class="py-1">testing</td>
+                </tr>
+                <tr>
+                    <th class="py-1 pr-2 font-medium whitespace-nowrap">Start Date:</th>
+                    <td class="py-1">Jun 20, 2025 11:01</td>
+                </tr>
+                <tr>
+                    <th class="py-1 pr-2 font-medium whitespace-nowrap">End Date:</th>
+                    <td class="py-1">Jun 21, 2025 11:01</td>
+                </tr>
+                <tr>
+                    <th class="py-1 pr-2 font-medium align-top whitespace-nowrap">Details:</th>
+                    <td class="py-1">asdsadasdasdsad</td>
                 </tr>
             </tbody>
         </table>
+
+
     </div>
 </div>
