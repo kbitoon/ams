aa2479165fecf359a3995a9d55d5bee64a3a3876
graceanvly <graceann151@gmail.com>
2025-09-12 13:41:40 +0800

    add route for electrical connection pdf

--- routes/web.php
+++ routes/web.php
@@ -113,6 +113,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/certificate/{id}/download', [CertificateController::class, 'downloadPdf'])->name('certificate.download');
 
     Route::get('/indigency/{id}/download', [CertificateController::class, 'indigencyPdf'])->name('indigency.download');
+    Route::get('/electrical/{id}/download', [CertificateController::class, 'electricalPdf'])->name('electrical.download');
 
 });
 
